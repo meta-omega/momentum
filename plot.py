0f6457eae193c67aea380fbca6c672680f0040d7
Dylan Fridman <dylanfridman@gmail.com>
2019-01-05 18:44:36 -0300

    Learned and tested how to plot live graphs

---
+++ plot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+def fitness(x):
+    x = x[0]
+    pos1 = x if x < 5 else 0
+    pos2 = x - 7.5 if 10 <= x < 20 else 0
+    neg1 = -x/2 + 7.5 if 5 <= x < 10 else 0
+    neg2 = -x/2 + 22.5 if x >= 20 else 0
+    return pos1 + pos2 + neg1 + neg2
+
+arr = [fitness([y]) for y in range(-10, 30)]
+
+for i in range(10):
+    for i in range(-10, 30):
+        plt.clf()
+        plt.plot(list(range(-10, 30)), arr)
+        plt.scatter(i, fitness([i]))
+        plt.pause(0.05)
+
+plt.show()
